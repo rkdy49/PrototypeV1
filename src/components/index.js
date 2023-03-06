c9490569a112c11f379148c230e4aa04fe578e23
Devansh-kajve <devanshkajve123@gmail.com>
2023-03-06 23:56:35 +0530

    deleting to revert

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as Navbar} from './navbar/Navbar';
-export {default as Header} from './header/Header';
-export {default as Bids} from './bids/Bids';
-export {default as Footer} from './footer/Footer';
