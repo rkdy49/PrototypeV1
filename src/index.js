f220499e2842d090152fa6c606d61f4a8baf50ef
Rounak Dubey <rkdy49@gmail.com>
2023-03-13 18:26:40 +0530

    Update index.js
    added server url

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { MoralisProvider } from "react-moralis";
 ReactDOM.render(
   <BrowserRouter>
     <MoralisProvider
-      serverUrl={process.env.REACT_APP_SERVER_URL ?? ''}
-      appId= {process.env.REACT_APP_APPLICATION_ID ?? ''}
+      serverUrl= 'https://gearfi-server.onrender.com/server'
+      appId= '001'
     >
       <App />
     </MoralisProvider>
