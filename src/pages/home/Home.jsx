cee12d3d8cd5cc81638e13170deb1ba0e7094f26
Krishna <krishnaagrawal2992@gmail.com>
2023-05-21 01:28:47 +0530

    second chart modification

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import {Bids, Footer, Header, } from '../../components'
+import React from 'react'
+import { Bids, Footer, Header } from '../../components'
 import Chart from '../../components/chart/Chart.jsx'
 
-
 const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Bids title='Hot Bids' />
+      <Footer />
+    </div>
+  )
+}
 
-  return <div>
-    <Chart />
-  </div>;
-};
-{/* <Header />
-<Bids title="Hot Bids"  />
-<Footer /> */}
-
-export default Home;
+export default Home
