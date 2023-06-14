2c26cbd2b834309c2e78ddae27257cc9f350828e
Krishna <krishnaagrawal2992@gmail.com>
2023-06-15 02:49:35 +0530

    frontend phase2 started and staking page added

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from "react";
-import data from "./data.json";
-import nft from "../../assets/nft.jpg";
-import { Link } from "react-router-dom";
-
-//console.log('prime: ', data)
+import { useEffect, useState } from 'react'
+import data from '../../assets/data.json'
+import nft from '../../assets/nft.jpg'
+import { useNavigate } from 'react-router-dom'
 
 const UserData = ({ users }) => {
+  const navigate = useNavigate()
+
+  const handleRedirect = (address) => {
+    navigate(`/collection/${address}`)
+  }
+
   return (
     <>
-   
       {users.map((curUser) => {
         const {
           COLLECTION,
@@ -19,71 +22,69 @@ const UserData = ({ users }) => {
           sevenD_VOLUME,
           OWNERS,
           SUPPLY,
-        } = curUser;
-        const red = oneD_CHANGE < 0;
+        } = curUser
+        const red = oneD_CHANGE < 0
         return (
           <tr
             key={curUser.address}
-            className="hover:bg-gray-800 text-center mt-4"
+            className='hover:bg-gray-800 text-center mt-4'
+            onClick={() => handleRedirect(curUser.address)}
+            style={{ cursor: 'pointer' }}
           >
-            <td className="p-2 ">
-              <Link to={`/collection/${curUser.address}`}>
-                <div className="flex items-center">
-                  <img src={curUser.img} className="w-16 rounded-full m-4" />
-                  <p className="text-md">{COLLECTION}</p>
-                </div>
-              </Link>
+            <td className='p-2 text-start'>
+              <div className='flex items-center'>
+                <img src={curUser.img} className='w-16 rounded-full m-4' />
+                <p className=''>{COLLECTION}</p>
+              </div>
             </td>
-
-            <td className="">{FLOOR_PRICE}</td>
-            <td className={` ${red ? "text-orange-700" : "text-white"}`}>
+            <td className=''>{FLOOR_PRICE}</td>
+            <td className={` ${red ? 'text-orange-700' : 'text-white'}`}>
               {oneD_CHANGE}
             </td>
-            <td className="p-4">{sevenD_CHANGE}</td>
-            <td className="p-4">{oneD_VOLUME}</td>
-            <td className="p-4">{sevenD_VOLUME}</td>
-            <td className="p-4">{OWNERS}</td>
-            <td className="p-4">{SUPPLY}</td>
+            <td className='p-4'>{sevenD_CHANGE}</td>
+            <td className='p-4'>{oneD_VOLUME}</td>
+            <td className='p-4'>{sevenD_VOLUME}</td>
+            <td className='p-4'>{OWNERS}</td>
+            <td className='p-4'>{SUPPLY}</td>
           </tr>
-        );
+        )
       })}
     </>
-  );
-};
+  )
+}
 
 const Chart = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
 
   const fetchUser = async () => {
     try {
       if (data.length > 0) {
-        setUsers(data);
+        setUsers(data)
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   useEffect(() => {
-    fetchUser();
-  }, []);
+    fetchUser()
+  }, [])
 
   return (
     <>
-    <h1 className="ml-20 p-4 text-2xl">Top Collections</h1>
-      <div className=" flex justify-center mt-20">
-      
-        <table className="text-sm w-[80%]">
+      <h1 className='ml-20 p-4 text-2xl'>Top Collections</h1>
+      <div className=' flex justify-center mt-20'>
+        <table className='text-sm w-[80%]'>
           <thead>
             <tr>
-              <th className="text-start pl-4">COLLECTION</th>
-              <th className="">FLOOR PRICE</th>
-              <th className="">1D CHANGE</th>
-              <th className="">7D CHANGE</th>
-              <th className="">1D VOLUME</th>
-              <th className="">7D VOLUME</th>
-              <th className="">OWNERS</th>
-              <th className="">SUPPLY</th>
+              <th className='text-start pl-4'>COLLECTION</th>
+              <th className=''>FLOOR PRICE</th>
+              <th className=''>1D CHANGE</th>
+              <th className=''>7D CHANGE</th>
+              <th className=''>1D VOLUME</th>
+              <th className=''>7D VOLUME</th>
+              <th className=''>OWNERS</th>
+              <th className=''>SUPPLY</th>
             </tr>
           </thead>
           <tbody>
@@ -92,7 +93,7 @@ const Chart = () => {
         </table>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Chart;
+export default Chart
--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -10,7 +10,7 @@ import './banner.css'
 
 export default function Banner() {
   // const [mouseHover, setMouseHover] = useState('')
-  
+
   // const showImage = (img) => {
   //     return(
   //         <img src={img} onMouseEnter={setMouseHover(true)} onMouseLeave={} />
@@ -24,51 +24,20 @@ export default function Banner() {
         <div className='mb-2 ml-4 '>By Kanpai Pandas</div>
         <div className='ml-4 flex flex-row mb-2'>
           <div className='mr-4'>
-            Floor Price<br/>
+            Floor Price
+            <br />
             1.40
           </div>
           <div className='mr-4'>
-            1D Volume<br/>
+            1D Volume
+            <br />
             9.77
           </div>
           <div>
             <button className='m-0'>View Collection👉</button>
           </div>
         </div>
-        <div className='flex justify-start m-4 z-0'>
-          <div className='flex-shrink-0 w-1/4'>
-            <div className=''>
-              <img
-                src={"https://i.seadn.io/gae/5c-HcdLMinTg3LvEwXYZYC-u5nN22Pn5ivTPYA4pVEsWJHU1rCobhUlHSFjZgCHPGSmcGMQGCrDCQU8BfSfygmL7Uol9MRQZt6-gqA?auto=format&dpr=1&w=1200"}
-                className='hover:scale-105 w-full px-8 transform origin-center transition-transform duration-300 object-contain mt-4'
-              />
-            </div>
-          </div>
-          <div className='flex-shrink-0 w-1/4'>
-            <div className='aspect-w-1 aspect-h-1'>
-              <img
-                src={nft_2}
-                className='hover:scale-105 w-full px-8 transform origin-center transition-transform duration-300 object-contain mt-4'
-              />
-            </div>
-          </div>
-          <div className='flex-shrink-0 w-1/4'>
-            <div className='aspect-w-1 aspect-h-1'>
-              <img
-                src={nft_2}
-                className='hover:scale-105 w-full px-8 transform origin-center transition-transform duration-300 object-contain mt-4 rounded-2xl'
-              />
-            </div>
-          </div>
-          <div className='flex-shrink-0 w-1/4 rounded-3xl'>
-            <div className='aspect-w-1 aspect-h-1 rounded-3xl'>
-              <img
-                src={nft_2}
-                className='hover:scale-105 w-full px-8 transform origin-center transition-transform duration-300 object-contain mt-4 '
-              />
-            </div>
-          </div>
-        </div>
+      
       </div>
     </div>
   )
