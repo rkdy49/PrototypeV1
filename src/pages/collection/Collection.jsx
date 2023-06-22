4925ea4076435e1cccd5d7730ca267b977b215bb
Krishna <krishnaagrawal2992@gmail.com>
2023-06-22 21:56:22 +0530

    images rendered on collection page

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -5,111 +5,41 @@ import bannerImg from '../../assets/banner2.avif'
 import { AiOutlineArrowRight } from 'react-icons/ai'
 import axios from 'axios'
 import { useParams } from 'react-router'
-// import data from '../../assets/data.json'
+import data from './data.json'
 
 import { UserContext } from '../../components/UserContext/UserContext'
 
 export default function Collection() {
-  // useEffect(() => {
-  //   axios.post(`http://localhost:8000/addListing`, {nftId, nftAddress, sellerAddress, metadata, price}).then(({ data }) => {
-  //     console.log("data is", data);
-  //   })
-  // }, [])
-  // useEffect(() => {
-  //   console.log('krishna')
-  //   axios(`http://localhost:8000/getListing/${collAddress}`, {collAddress}).then(
-  //     ({ data }) => {
-  //       console.log('data is', data)
-  //     }
-  //   )
-  // }, [])
-  // useEffect(() => {
-  //   console.log('krishna')
-  // axios(`http://localhost:8000/collection/${collAddress}`).then(
-  //   ({ data }) => {
-  //     console.log('data is', data)
-  //   }
-  // )
-  // }, [])
-  // useEffect(() => {
-  //   axios('http://localhost:3000/getNFTMetadata').then((data) => {
-  //     // console.log("data is: ", data.data.metadata)
-  //     fetch(data.data)
-  //       .then((response) => {
-  //         if (!response.ok) {
-  //           throw new Error('Network response was not ok')
-  //         }
-  //         return response.json() // Extract JSON data from the response
-  //       })
-  //       .then((result) => {
-  //         console.log('data', result.image)
-  //       })
-  //       .catch((error) => {
-  //         console.error('An error occurred:', error)
-  //       })
-  //   })
-  // }, [])
-
-  // data.map(coll => {
-  //   console.log("coll is", coll)
-  // })
-  // console.log('data', data)
-  // const response = data.find((entry) => entry.address===collAddress)
-
-  // console.log('response', response)
-
   const { collAddress } = useParams()
-  const [nftIds, setNftIds] = useState([])
   const [nftData, setNftData] = useState([])
 
   useEffect(() => {
     console.log('useEffect1')
-    axios(`http://localhost:8000/collection/${collAddress}`).then(
+    axios(`http://localhost:8000/assets/${collAddress}`).then(
       ({ data }) => {
-        console.log('datacollection type', typeof data.data.collection[0].nfts)
-        setNftIds(data.data.collection[0].nfts)
+        console.log('datacollection type', data)
+        setNftData(data)
       }
     )
   }, [collAddress])
-
-  useEffect(() => {
-    console.log('useEffect2')
-    const fetchNftData = async () => {
-      const fetchData = async (nftId) => {
-        const response = await fetch(`http://localhost:8000/nft/${nftId}`)
-        const { data } = await response.json()
-        return data.nft[0]
-      }
-
-      console.log('nftid type', typeof nftIds)
-
-      const promises = nftIds.map((nftId) => fetchData(nftId))
-      
-      const fetchedData = await Promise.all(promises)
-
-      setNftData(fetchedData)
-    }
-
-    if (nftIds.length > 0) {
-      fetchNftData()
-    }
-  }, [nftIds])
-
-  console.log('nftIds:', nftIds)
   console.log('nftData:', nftData)
 
   const nftDataArr = Object.values(nftData);
 
-  nftDataArr.map((nft) => console.log("fdhg",nft))
+  const collData = data.find((obj) => {
+    return obj.address === collAddress
+  });
+  console.log('colldata', collData)
 
   return (
-    <div className='text-white'>
+    <div className='text-white mt-20'>
       <div className='relative'>
-        {/* <img src={response.bannerImg} alt='' className='banner_img relative top-0 left-0 w-full' /> */}
-        {/* <img
-          src={response.img}
+        <img src={collData.banner_img} alt='' className='object-cover h-[40vh] relative top-0 left-0 w-full' />
+        <img
+          src={collData.img}
+          alt = ''
           className='b-4 absolute -bottom-4 left-20 border-4 w-20 sm:w-24 md:w-32 lg:w-40 border-white rounded-lg '
-        /> */}
+        />
       </div>
       <div>
         <div className='mx-4 mb-0 mt-8 text-3xl font-bold'>
@@ -147,11 +77,11 @@ export default function Collection() {
           {nftDataArr.map((nft) => {
             return (
               <div className='flex flex-col m-2 mt-8 w-56 h-72 border-2 border-indigo-300'>
-                {console.log('image url', nftData.image)}
-                <img src={nft.image} alt='' className='w-64 h-48' />
+                {console.log(`nft image is : https://ipfs.io/ipfs/${nft.metadata.imageURI.split('//')[1]}`)}
+                <img src={`https://ipfs.io/ipfs/${nft.metadata.imageURI.split('//')[1]}`} alt='' className='w-64 h-48' />
                 <div className='flex flex-col mt-0 p-4'>
                   <div className='font-bold mb-2 flex items-center justify-between'>
-                    <div>nft:{nft.name}</div>
+                    <div>{nft.metadata.name}</div>
                     <div>
                       <AiOutlineArrowRight className='mr-4' />
                     </div>
