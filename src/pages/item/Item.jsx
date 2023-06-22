bf0881f5f3353e7f8e60dbaa157b1b64b4ac087f
Krishna <krishnaagrawal2992@gmail.com>
2023-06-23 03:29:58 +0530

    added nft page

--- src/pages/item/Item.jsx
+++ src/pages/item/Item.jsx
@@ -1,95 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './item.css'
+import axios from 'axios'
 import LinedChart from '../../components/lineChart/LinedChart'
 
 import { NftMarketplace_address, abi_marketplace } from './../../constants'
 import { useLocation } from 'react-router'
+import { useParams } from 'react-router'
+
 
 import { ethers } from 'ethers'
 import { Link } from 'react-router-dom'
 
 const Item = () => {
-  // const { user, Moralis } = useMoralis();
-
+  const [nftData, setNftData] = useState([])
+  const { tokenAddress, tokenId } = useParams()
   const location = useLocation()
   const nft = location.state?.data
-  //console.log(nft.token_address._value);
   const [sale, setSale] = useState()
-  const [showBuyOpt, setShowBuyOpt] = useState(false)
 
-  // async function storeInitializedSale() {
-  //   const InitializedSale = Moralis.Object.extend("Sales");
+  useEffect(() => {
+    console.log('useEffect1')
+    axios(`http://localhost:8000/assets/${tokenAddress}/${tokenId}`).then(
+      ({ data }) => {
+        console.log('datacollection type', data)
+        setNftData(data)
+      }
+    )
+  }, [])
 
-  //   const initializedSale = new InitializedSale();
 
-  //   initializedSale.set("userAddress", user.id);
-  //   initializedSale.set("nft", nft);
-
-  //   await initializedSale.save();
-  // }
-
-  async function buy() {
-    const accounts = await window.ethereum.request({ method: 'eth_accounts' })
-    const chainId = await window.ethereum.request({ method: 'eth_chainId' })
-
-    if (accounts.length === 0) {
-      alert('Please connect Wallet')
-      return
-    }
-
-    if (chainId !== '0x5') {
-      alert('Please switch to Goerli Testnet')
-      return
-    }
-
-    if (window.ethereum) {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(
-        NftMarketplace_address,
-        abi_marketplace,
-        signer
-      )
-
-      const response = await contract.initializeSale(
-        nft.token_address._value,
-        nft.token_id,
-        {
-          value: ethers.utils.parseEther('0.003'),
-        }
-      )
-      // .then(() => {
-      //   storeInitializedSale();
-      //   setSale(true);
-      // });
-
-      console.log(response)
-    } else alert('Sorry no wallet found')
-  }
 
   return (
-    <div className='item flex px-6 text-white'>
+    <div className='item flex px-6 text-white mt-20'>
       <div className='item-image flex flex-col justify-center items-center border-r border-gray-200'>
+        {  console.log('nftDataaa', nftData.metadata?.imageURI)}
         <img
-          src={nft.metadata?.image.replace(
-            'ipfs://',
-            'https://ipfs.moralis.io:2053/ipfs/'
-          )}
+          src={`https://ipfs.io/ipfs/${nftData.metadata?.imageURI.split('//')[1]}`}
           alt=''
-          className='w-90p rounded-15'
+          className='rounded-15'
         />
         {/* <p className='mt-4 '>Description: {nft?.metadata.description}</p> */}
         <div className=' flex-col mt-4 w-full px-8'>
           <div className='p-4 border border-white border-b-0'>Description:</div>
-          <div className='p-4 border border-white'>
-            {nft?.metadata.description}
+          <div className='p-4 border border-white text-white'>
+            {nftData.metadata?.description}
           </div>
         </div>
       </div>
       <div className='item-content flex-1 flex justify-start items-start flex-col m-5 relative'>
         <div className='item-content-title'>
           <h1 className='font-bold text-28 '>
-            {nft?.metadata.name} #{nft.token_id}
+            {nftData.metadata?.name} #{nftData?.tokenId}
           </h1>
         </div>
 
@@ -111,35 +72,19 @@ const Item = () => {
               <div className='relative inline-block'>
                 <button
                   className='primary-btn mb-0'
-                  onClick={() => buy()}
-                  onMouseEnter={() => setShowBuyOpt(true)}
-                  onMouseLeave={() => setShowBuyOpt(false)}
                 >
                   Buy Now For 0.003 ETH
                 </button>
-                {showBuyOpt && (
-                  <div
-                    className='absolute end-0 left-0 mt-0 m-4 bg-black p-2 z-20 block border border-white'
-                    onMouseEnter={() => setShowBuyOpt(true)}
-                    onMouseLeave={() => setShowBuyOpt(false)}
-                  >
-                    <div className=' p-4 cursor-pointer hover:bg-slate-700'>
-                      buy now
-                    </div>
-                    <div className=' p-4 cursor-pointer hover:bg-slate-700'>
-                      buy later
-                    </div>
-                  </div>
-                )}
+                
               </div>
-              <button className='primary-btn' onClick={() => buy()}>
+              <button className='primary-btn'>
                 Make Offer
               </button>
             </div>
           )}
         </div>
-        <LinedChart />
-        <div className='m-4 mt-8 border border-white'>
+        {/* <LinedChart /> */}
+        {/* <div className='m-4 mt-8 border border-white'>
           Offers:
           <table className='w-full border border-white'>
             <thead>
@@ -159,53 +104,10 @@ const Item = () => {
                 <th className='font-semibold'>0.05ETH</th>
             </tbody>
           </table>
-        </div>
+        </div> */}
       </div>
     </div>
   )
 }
 
 export default Item
-
-{
-  /* <div className="item flex px-6">
-  <div className="item-image flex-1 flex justify-center items-center border-r border-gray-200">
-    <img
-      src={nft.metadata?.image.replace(
-        "ipfs://",
-        "https://ipfs.moralis.io:2053/ipfs/"
-      )}
-      alt=""
-      className="w-90p rounded-15"
-    />
-  </div>
-  <div className="item-content flex-1 flex justify-start items-start flex-col m-5 relative">
-    <div className="item-content-title">
-      <h1 className="font-bold text-28 ">
-        {nft?.metadata.name} #{nft.token_id}
-      </h1>
-    </div>
-
-    <div className="item-content-detail mt-5">
-      <p className="">
-        Description: {nft?.metadata.description}
-      </p>
-      <p className="">Owner: 0xB9e53abF5b0bAE6353076467F0505DebA8A98efa</p>
-      <p className="">Price: 0.01 ETH</p>
-      <p className="">Downpayment: 30%</p>
-      <p className="">Repayment Duration: 4 days</p>
-    </div>
-    <div className="item-content-buy">
-      {sale ? (
-        <Link to={`/profile/${user.id}`}>
-          <button className="primary-btn">Checkout Sale</button>
-        </Link>
-      ) : (
-        <button className="primary-btn" onClick={() => buy()}>
-          Buy Now For 0.003 ETH
-        </button>
-      )}
-    </div>
-  </div>
-</div>  */
-}
